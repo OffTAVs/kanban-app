61ad615a299d20bf22f7fd4fc1517fdf9b3ed915
Otavio~ <otaviobarbosa2004@gmail.com>
2025-06-23 16:29:58 -0300

    feat: create middleware

---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+
+export async function middleware(req) {
+  const res = NextResponse.next()
+
+  const supabase = createMiddlewareClient({ req, res })
+
+  
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+
+  if (!session && req.nextUrl.pathname.startsWith('/organizations')) {
+    
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  
+  return res
+}
+
+// Define para quais rotas o middleware é aplicado
+export const config = {
+  matcher: [
+    '/dashboard/:path*'],
+}
