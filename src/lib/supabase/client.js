977fe616ed02df2ce478182035172df2a7480ccf
Otavio~ <otaviobarbosa2004@gmail.com>
2025-06-23 17:00:48 -0300

    fix middleware, client and server

--- src/lib/supabase/client.js
+++ src/lib/supabase/client.js
@@ -1,4 +1,4 @@
-'use client'
+
 
 import { createPagesBrowserClient} from '@supabase/auth-helpers-nextjs'
 
--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+
 
 export async function middleware(req) {
   const res = NextResponse.next()
